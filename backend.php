7d339b15341c2dc8c3bc3627070b8784ee3776b4
shaun gilchrist <shaunxcode@gmail.com>
2011-07-08 22:44:12 -0600

    adding test

--- backend.php
+++ backend.php
@@ -32,18 +32,19 @@
   // infinite loop until the data file is not modified
   $lastmodif    = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0;
   $currentmodif = filemtime($filename);
-  while ($currentmodif <= $lastmodif) // check if the data file has been modified
+/*  while ($currentmodif <= $lastmodif) // check if the data file has been modified
   {
     usleep(10000); // sleep 10ms to unload the CPU
     clearstatcache();
     $currentmodif = filemtime($filename);
-  }
- 
+  }*/
+
   // return a json array
   echo json_encode(array(
     'messages' => json_decode('[' . substr(file_get_contents($filename), 0, -2) .']'), 
     'timestamp' => $currentmodif));
 
-  flush();
+  die();
+  //flush();
  
   ?>
